d59a42ad75b00dc56cbd1f77a7e158281fc30266
pangye202264690373 <202264690373@mail.scut.edu.cn>
2025-11-29 11:39:12 +0800

    Initial commit

--- train_rt1_traj.sh
+++ train_rt1_traj.sh
@@ -1,7 +1,7 @@
 export MODEL_DIR="./PhotoDoodle_Pretrain" # you may need to modity this in order to train your own model
-export OUTPUT_DIR="outputs/rt1_traj_7000"
+export OUTPUT_DIR="outputs/rt1_traj"
 export CONFIG="./default_config.yaml"
-export TRAIN_DATA="/root/PhotoDoodle/data/rt1_merge_traj_7000/pairs_train_text_inpaint.jsonl"
+export TRAIN_DATA="/root/PhotoDoodle/data/rt1_merge_traj/pairs_train_text_inpaint.jsonl"
 export LOG_PATH="$OUTPUT_DIR/log"
 
 CUDA_VISIBLE_DEVICES=0 accelerate launch --config_file $CONFIG train_lora_flux_pe.py \
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=0 accelerate launch --config_file $CONFIG train_lora_flux_p
     --learning_rate=1e-4 \
     --train_batch_size=1 \
     --num_validation_images=2 \
-    --validation_image "/root/PhotoDoodle/data/rt1_merge_traj_7000/005347_ff.png" \
+    --validation_image "/root/PhotoDoodle/data/rt1_merge_traj/005347_ff.png" \
     --validation_prompt "A 3×3 grid image to inpaint the blank black cells: only the top-left cell shows the real scene with arrows indicating the start and end of the trajectory." \
     --num_train_epochs=10 \
     --validation_steps=2000 \
